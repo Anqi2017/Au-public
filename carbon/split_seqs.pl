2b87a455527283bcefd733b18b59ab6d46a13285
Benjamin Deonovic <bdeonovic@gmail.com>
2014-10-22 07:30:17 -0500

    Add functionality to split_seqs.pl

--- carbon/split_seqs.pl
+++ carbon/split_seqs.pl
@@ -3,10 +3,12 @@ use warnings;
 use strict;
 use Getopt::Long;
 
-my $fastq_flag;
+my $fastq_flag = '';
 my $max_length;
-GetOptions("fastq=i"  => \$fastq_flag, 
-           "length=i" => \$max_length
+my $tsv_flag = '';
+GetOptions("length=i" => \$max_length,
+           "fastq"    => \$fastq_flag, 
+           "tsv"      => \$tsv_flag
 ) or die("Error in command line arguments\n");
 
 
@@ -17,32 +19,58 @@ my @b;
 my $quot;
 my $rem;
 
-my @seq;
 while($line = <>){
   chomp $line;
   @a = split(/\t/,$line);
-
-  if($fastq_flag){
-    die("Not yet implemented\n");
-  }else{
-    @seq = split("", $a[1]);
-    $length = length($a[1]);
-    #print STDERR "ON: $a[0], max length: $max_length, length: $length\n";
-    if($length > $max_length){
-      $quot = int( $length / $max_length);
-      $rem  = $length % $max_length; 
-      for(my $i=0; $i < $quot; $i++){
-        #print STDERR "SPLITTING: $a[0], iter $i\n";
-        #print STDERR substr($a[1], $i*$max_length, $max_length), "\n";
-        push(@b, "$a[0]_$i\t".substr($a[1], $i*$max_length, $max_length)); 
+  $length = length($a[1]);
+  @b = ();
+  if($length > $max_length){
+    $quot = int( $length / $max_length);
+    $rem  = $length % $max_length; 
+    for(my $i=0; $i < $quot; $i++){
+      if( $fastq_flag ){
+        if( $tsv_flag){
+          push(@b, "$a[0]_$i\t".substr($a[1], $i*$max_length, $max_length)."\t$a[2]\t".substr($a[3],$i*$max_length, $max_length)); 
+        }else{
+          push(@b, "$a[0]_$i\n".substr($a[1], $i*$max_length, $max_length)."\n$a[2]\n".substr($a[3],$i*$max_length, $max_length)); 
+        }
+      }else{
+        if( $tsv_flag){
+          push(@b, "$a[0]_$i\t".substr($a[1], $i*$max_length, $max_length)); 
+        }else{
+          push(@b, ">$a[0]_$i\n".substr($a[1], $i*$max_length, $max_length)); 
+        }
       }
-      if( $rem > 0 ){
-        push(@b, "$a[0]_".$quot."\t".substr($a[1], $max_length*$quot, $rem));
-      }
-      print join("\n",@b), "\n";
-    }else{
-      print "$a[0]\t$a[1]\n";
     }
-
+    if( $rem > 0 ){
+      if( $fastq_flag){
+        if( $tsv_flag){
+         push(@b, "$a[0]_".$quot."\t".substr($a[1], $max_length*$quot, $rem)."\t$a[2]\t".substr($a[3],$quot*$max_length, $rem));
+        }else{
+         push(@b, "$a[0]_".$quot."\n".substr($a[1], $max_length*$quot, $rem)."\n$a[2]\n".substr($a[3],$quot*$max_length, $rem));
+        }
+      }else{
+        if( $tsv_flag){
+         push(@b, "$a[0]_".$quot."\t".substr($a[1], $max_length*$quot, $rem));
+        }else{
+         push(@b, ">$a[0]_".$quot."\n".substr($a[1], $max_length*$quot, $rem));
+        }
+      }
+    }
+    print join("\n",@b), "\n";
+  }else{
+    if( $fastq_flag ){
+      if( $tsv_flag){
+        print "$a[0]\t$a[1]\t$a[2]\t$a[3]\n";
+      }else{
+        print "$a[0]\n$a[1]\n$a[2]\n$a[3]\n";
+      }
+    }else{
+      if( $tsv_flag){
+        print "$a[0]\t$a[1]\n";
+      }else{
+        print ">$a[0]\n$a[1]\n";
+      }
+    }
   }
 }
