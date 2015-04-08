36c7f9dfaca0171162c5e5c896ad9f61f76b1571
Jason L Weirather <jason.weirather@gmail.com>
2015-04-08 14:43:26 -0500

    change A and B genepreds into positional required arguments to make the help file look a little more presentable.

--- iron/utilities/genepred_intersect.py
+++ iron/utilities/genepred_intersect.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
 import GenePredBasics, RangeBasics
 import argparse, sys
+from math import log, pow
 
 # This should be a powerful commandline utility understanding the overlaps of genepred files
+# Prioritize partial matches based on 
+# first - exons... more consecutive shared exons the better
+# second - overlapping length... more overlapping length the better
+# third - overlapping fractions ... the more complete the overlap the better
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('-a',required=True,help='FILENAME genepred file A')
-  parser.add_argument('-b',required=True,help='FILENAME genepred file B')
+  parser.add_argument('a',nargs=1,help='FILENAME genepred file A')
+  parser.add_argument('b',nargs=1,help='FILENAME genepred file B')
   parser.add_argument('--minexoncount',nargs='?',help='INT the minimum number of exons required.')
-  parser.add_argument('--leftouterjoin',nargs='?',help='OUTPUT the entry A regardless of whether a matching entry in B is found')
   parser.add_argument('--minoverlap_internal',nargs='?',help='FLOAT the fraction (0-1) of the required reciprocal overlap of an internal exon to call an exon a match.')
   parser.add_argument('--minoverlap_first',nargs='?',help='FLOAT the fraction (0-1) of the required reciprocal overlap of the first exon to call an exon a match.')
   parser.add_argument('--minoverlap_last',nargs='?',help='FLOAT the fraction (0-1) of the required reciprocal overlap of the last exon to call an exon a match.')
   parser.add_argument('--minoverlap',nargs='?',help='FLOAT the fraction (0-1) of the required reciprocal overlap of any exon to call an exon a match.')
+  parser.add_argument('--leftouterjoin',action='store_true',help='Output the entry A regardless of whether a matching entry in B is found')
   parser.add_argument('--output_a_not_in_b',action='store_true',help='Output entries that occur in A but not B')
   parser.add_argument('--best_b_only',action='store_true',help='Output only one entry of B for each A and try to pick the best based on reciprocal overlap')
-  parser.add_argument('--allow_a_subset_of_b_fragments',action='store_true',help='If A is just a subset of B, then call it as a match.')
+  parser.add_argument('--allow_a_subset_of_b_fragments',action='store_true',help='If A is just a subset of B, then call it as a match.  This means all exons of A found a conecutive match, but B could have more exons on either end.')
   parser.add_argument('--allow_any_fragments',action='store_true',help='If set, allow any partial match, not just the best')
   args = parser.parse_args()
   
@@ -32,14 +37,15 @@ def main():
     overlap[1] = float(args.minoverlap_internal)
 
   # read the genepred files
-  gpdA = GenePredBasics.GenePredFile(args.a)
-  gpdB = GenePredBasics.GenePredFile(args.b)
+  gpdA = GenePredBasics.GenePredFile(args.a[0])
+  gpdB = GenePredBasics.GenePredFile(args.b[0])
 
   for eA in gpdA.entries:
     a_unique = True
     best_exon_count = 0
     best_overlap = 0
     best_line = ''
+    best_frac = 0
     for eB in gpdB.entries:
       double_line = GenePredBasics.entry_to_line(eA.entry) + "\t" + GenePredBasics.entry_to_line(eB.entry) + "\n"
       gpd_comparison = GenePredBasics.GenePredComparison()
@@ -58,11 +64,14 @@ def main():
             # only do the best
             if gpd_comparison.output['consecutive_exons'] > best_exon_count \
             or (gpd_comparison.output['consecutive_exons'] == best_exon_count \
-            and gpd_comparison.output['overlap_length'] > best_overlap):
+            and gpd_comparison.output['overlap_length'] > best_overlap) \
+            or (gpd_comparison.output['consecutive_exons'] == best_exon_count \
+            and gpd_comparison.output['overlap_length'] == best_overlap \
+            and harmonic_mean(gpd_comparison.output['overlap_fractions']) > best_frac):
               best_exon_count = gpd_comparison.output['consecutive_exons']
               best_overlap = gpd_comparison.output['overlap_length']
               best_line = double_line
-
+              best_frac = harmonic_mean(gpd_comparison.output['overlap_fractions'])
       # Allow partial matches
       else:          
         gpd_comparison.compare(eA,eB)
@@ -81,14 +90,26 @@ def main():
           else:
             if gpd_comparison.output['consecutive_exons'] > best_exon_count \
             or (gpd_comparison.output['consecutive_exons'] == best_exon_count \
-            and gpd_comparison.output['overlap_length'] > best_overlap):
+            and gpd_comparison.output['overlap_length'] > best_overlap) \
+            or (gpd_comparison.output['consecutive_exons'] == best_exon_count \
+            and gpd_comparison.output['overlap_length'] == best_overlap \
+            and harmonic_mean(gpd_comparison.output['overlap_fractions']) > best_frac):
               best_exon_count = gpd_comparison.output['consecutive_exons']
               best_overlap = gpd_comparison.output['overlap_length']
               best_line = double_line
+              best_frac = harmonic_mean(gpd_comparison.output['overlap_fractions'])
     if best_exon_count > 0 and args.best_b_only:
       sys.stdout.write(best_line)
     if a_unique and (args.output_a_not_in_b or args.leftouterjoin):
       sys.stdout.write(GenePredBasics.entry_to_line(eA.entry)+"\n")
+
+def harmonic_mean(inlist):
+  total = 0
+  if len(inlist) == 0: return 0
+  for x in inlist:
+    if x == 0:  return 0
+    total += log(x,2)
+  return pow(2,total)
 
 def compare(eA,eB,overlap):
   range_list_A = eA.range_set.get_range_list()
