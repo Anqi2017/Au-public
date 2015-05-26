61a3736fca88f540c3546ef689753d2298c7a58a
Jason L Weirather <jason.weirather@gmail.com>
2015-05-26 15:59:13 -0500

    modified psl to target genepred to take STDIN

--- iron/utilities/psl_to_target_genepred.py
+++ iron/utilities/psl_to_target_genepred.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, inspect
+import sys, os, inspect, argparse
 
 #pre: psl filename, an optional integer for smoothing size
 #     The smoothing is done to combine adjacent "exons" within that size definition to get rid of small deletions or indels
@@ -11,20 +11,28 @@ pythonfolder_loc = "../pythonlib"
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],pythonfolder_loc)))
 if cmd_subfolder not in sys.path:
   sys.path.insert(0,cmd_subfolder)
-import psl_basics, genepred_basics
+
+import PSLBasics, GenePredBasics
 
 
 def main():
-  if len(sys.argv) < 2:
-    print sys.argv[0] + ' <psl filename> <smoothing size (optional)>'
-    sys.exit()
-  pslfilename = sys.argv[1]
-  smooth_size = 0
-  if len(sys.argv) == 3:
-    smooth_size = int(sys.argv[2])
-  with open(pslfilename) as infile:
+  parser = argparse.ArgumentParser(description="Convert a psl file into a target formated genepred file.")
+  parser.add_argument('--fill_gaps',type=int,default=0,help="Close gaps this size or smaller.")
+  parser.add_argument('input_name',help="Input PSL file, use - to indicate STDIN.")
+  args = parser.parse_args()
+  
+  pslfilehandle = sys.stdin
+  if args.input_name != '-':
+    pslfilehandle = open(args.input_name)
+  with pslfilehandle as infile:
     for line in infile:
-      psl_entry = psl_basics.read_psl_entry(line)
-      genepred_line = psl_basics.convert_entry_to_genepred_line(psl_entry)
+      psl_entry = PSLBasics.line_to_entry(line)
+      genepred_line = PSLBasics.convert_entry_to_genepred_line(psl_entry)
+      if args.fill_gaps > 0:
+        genepred_entry = GenePredBasics.line_to_entry(genepred_line)
+        genepred_entry2 = GenePredBasics.smooth_gaps(genepred_entry,args.fill_gaps)
+        genepred_line = GenePredBasics.entry_to_line(genepred_entry2)
       print genepred_line
-main()
+
+if __name__=="__main__":
+  main()
