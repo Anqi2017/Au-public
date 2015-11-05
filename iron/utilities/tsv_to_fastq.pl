afecd171820f6b77d8debb4d09841ad5da236b5a
Jason L Weirather <jason.weirather@gmail.com>
2015-11-05 11:48:13 -0600

    fix returns in fastq

--- iron/utilities/tsv_to_fastq.pl
+++ iron/utilities/tsv_to_fastq.pl
@@ -3,5 +3,5 @@ use strict;
 while(my $line = <STDIN>) {
   chomp($line);
   my ($name,$f2,$f3,$f4) = split(/\t/,$line);
-  print "@"."$name\n$f2\t$f3\t$f4\n";
+  print "@"."$name\n$f2\n$f3\n$f4\n";
 }
