fbb8ab1797243591bcdc229e8ae3e131f1207727
Jason L Weirather <jason.weirather@gmail.com>
2015-11-06 10:46:21 -0600

    switching sorters to a python script to give neater options for temporary directory setting and input output or stream settings

--- iron/utilities/sort_fasta.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-#Pre: A fasta file streamed in STDIN
-#Post: A fasta file ordered by the sequence name streamed to STDOUT
-
-open(STREAM,"| fasta_to_tsv.pl | sort -k 1,1 | tsv_to_fasta.pl > /dev/stdout") or die;
-while(my $line = <STDIN>) {
-  print STREAM $line;
-}
--- iron/utilities/sort_fastq.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-#Pre: A fastq file streamed in STDIN
-#Post: A fastq file ordered by the sequence name streamed to STDOUT
-
-open(STREAM,"| fastq_to_tsv.pl | sort -k 1,1 | tsv_to_fastq.pl > /dev/stdout") or die;
-while(my $line = <STDIN>) {
-  print STREAM $line;
-}
