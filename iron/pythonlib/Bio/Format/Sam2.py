37238b57be0282d4ec8928be88ee099e143d02e2
Jason L Weirather <jason.weirather@gmail.com>
2016-04-17 00:53:00 -0500

    adding random access hooks

--- iron/pythonlib/Bio/Format/Sam2.py
+++ iron/pythonlib/Bio/Format/Sam2.py
@@ -1,16 +1,54 @@
-import struct, zlib, sys, re
+import struct, zlib, sys, re, os, gzip
 from cStringIO import StringIO
 from string import maketrans
+from Bio.Range import GenomicRange
 _bam_ops = maketrans('012345678','MIDNSHP=X')
 _bam_char = maketrans('abcdefghijklmnop','=ACMGRSVTWYHKDBN')
 _bam_value_type = {'c':[1,'<b'],'C':[1,'<B'],'s':[2,'<h'],'S':[2,'<H'],'i':[4,'<i'],'I':[4,'<I']}
+_sam_cigar_target_add = re.compile('[MI=XDN]$')
 
 # A sam entry
 class SAM:
   def __init__(self,line=None,dict=None):
-    self._entries = {}
     self._line = None
+    self._target_range = None
+    self._private_values = SAM.PrivateValues()
+    # Private values holds tags cigar and entries
     return
+  def __str__(self):
+    if self._line:
+      return self._line
+    return self.get_line()
+  def get_target_range(self):
+    if not self.is_aligned(): return None
+    if self._target_range: return self._target_range.length()
+    global _sam_cigar_target_add
+    tlen = sum([x[0] for x in self.get_cigar() if _sam_cigar_target_add.match(x[1])])
+    self._target_range = GenomicRange(self.value('rname'),self.value('pos'),self.value('pos')+tlen-1)
+    return self._target_range
+  def check_flag(self,inbit):
+    if self.value('flag') & inbit: return True
+    return False
+  def is_aligned(self):
+    return not self.check_flag(0x4)
+
+  #assemble the line if its not there yet
+  def get_line(self):
+    if not self._line:
+      p = self.value('pos')
+      if p == 0: p = '*'
+      else: p = str(p)
+      self._line = self.value('qname')+"\t"+str(self.value('flag'))+"\t"+self.value('rname')+"\t"+p+"\t"+str(self.value('mapq'))+"\t"+self.value('cigar')+"\t"+self.value('rnext')+"\t"+str(self.value('pnext'))+"\t"+str(self.value('tlen'))+"\t"+self.value('seq')+"\t"+self.value('qual')
+      if self.value('remainder'):
+        self._line += "\t"+self.value('remainder')
+    return self._line
+  def value(self,key):
+    return self._private_values.get_entry(key)
+  def get_tags(self): 
+    return self._private_values.get_tags()
+  def get_cigar(self): 
+    return self._private_values.get_tags()
+
   #Bam files need a specific override to get_tags and get_cigar that would break other parts of the class if we 
   # access the variables other ways
   #Force tags and cigars to be hidden so we don't accidently change them.
@@ -18,18 +56,21 @@ class SAM:
     def __init__(self):
       self.__tags = None
       self.__cigar = None
+      self.__entries = {}
     def set_tags(self,tags): self.__tags=tags
     def get_tags(self): return self.__tags
     def set_cigar(self,cigar): self.__cigar=cigar
     def get_cigar(self): return self.__cigar
-  def get_tags(self): 
-    return self._private_values.get_tags()
-  def get_cigar(self): 
-    return self._private_values.get_tags()
-  def __str__(self):
-    return self._line
-  def value(self,key):
-    return self._entries[key]
+    def set_entries_dict(self,mydict): self.__entries = mydict # set the entire dictionary at once
+    def get_entry(self,key): 
+      if key not in self.__entries:
+        sys.stderr.write("WARNING: key "+str(key)+"not in entries\n")
+        return None
+      return self.__entries[key]
+    def is_entry_key(self,key):  
+      if key in self.__entries:  return True
+      return False
+    def set_entry(self,key,value): self.__entries[key] = value
 
 # Very much like a sam entry but optimized for access from a bam
 # Slows down for accessing things that need more decoding like
@@ -37,10 +78,11 @@ class SAM:
 class BAM(SAM):
   def __init__(self,bin_data,ref_names,fileName=None,blockStart=None,innerStart=None):
     part_dict = _parse_bam_data_block(bin_data,ref_names)
-    self._entries = part_dict
     self._line = None
+    self._target_range = None
     self._file_position = {'fileName':fileName,'blockStart':blockStart,'innerStart':innerStart} # The most special information about the bam
     self._private_values = BAM.PrivateValues() # keep from accidently accessing some variables other than by methods
+    self._private_values.set_entries_dict(part_dict)
     return
 
   def get_filename(self):
@@ -56,51 +98,78 @@ class BAM(SAM):
   def get_tags(self): 
     cur = self._private_values.get_tags()
     if not cur:
-      v1,v2 = _bin_to_extra(self._entries['extra_bytes'])
+      v1,v2 = _bin_to_extra(self.value('extra_bytes'))
       self._private_values.set_tags(v1) #keep the cigar array in a special palce
-      self._entries['remainder'] = v2
+      self._private_values.set_entry('remainder',v2)
     return self._private_values.get_tags()
   def get_cigar(self): 
     cur = self._private_values.get_cigar()
     if not cur:
-      v1,v2 = _bin_to_cigar(self._entries['cigar_bytes'])
+      v1,v2 = _bin_to_cigar(self.value('cigar_bytes'))
       self._private_values.set_cigar(v1) #keep the cigar array in a special palce
-      self._entries['cigar'] = v2
+      self._private_values.set_entry('cigar',v2)
     return self._private_values.get_cigar()
-
-  def __str__(self):
-    if not self._line:
-      if self.value('rnext') == self.value('rname'): self._entries['rnext'] = '='
-      self._line = self.value('qname')+"\t"+str(self.value('flag'))+"\t"+self.value('rname')+"\t"+str(self.value('pos'))+"\t"+str(self.value('mapq'))+"\t"+self.value('cigar')+"\t"+self.value('rnext')+"\t"+str(self.value('pnext'))+"\t"+str(self.value('tlen'))+"\t"+self.value('seq')+"\t"+self.value('qual')
-      if self.value('remainder'):
-        self._line += "\t"+self.value('remainder')
-    return self._line
 
 
   def value(self,key):
-    if key not in self._entries:
+    if not self._private_values.is_entry_key(key):
       if key == 'seq':
-        v = _bin_to_seq(self._entries['seq_bytes'])
-        self._entries['seq'] = v
+        v = _bin_to_seq(self.value('seq_bytes'))
+        self._private_values.set_entry('seq',v)
         return v
       elif key == 'qual':
-        v = _bin_to_qual(self._entries['qual_bytes'])
-        self._entries['qual'] = v
+        v = _bin_to_qual(self.value('qual_bytes'))
+        if not v: v = '*'
+        self._private_values.set_entry('qual',v)
         return v
       elif key == 'cigar':
-        v1,v2 = _bin_to_cigar(self._entries['cigar_bytes'])
+        v1,v2 = _bin_to_cigar(self.value('cigar_bytes'))
         self._private_values.set_cigar(v1) #keep the cigar array in a special palce
-        self._entries['cigar'] = v2
-        return self._entries[key]
+        self._private_values.set_entry('cigar',v2)
+        return v2
       elif key == 'remainder':
-        v1,v2 = _bin_to_extra(self._entries['extra_bytes'])
+        v1,v2 = _bin_to_extra(self.value('extra_bytes'))
         self._private_values.set_tags(v1) #keep the cigar array in a special palce
-        self._entries['remainder'] = v2
-        return self._entries[key]
-    return self._entries[key]
+        self._private_values.set_entry('remainder',v2)
+        return v2
+    return self._private_values.get_entry(key)
+
+class BAMIndex:
+  def __init__(self,index_file):
+    self.index_file = index_file
+    self._name_to_num = {}
+    self._num_to_name = {}
+    self._ranges = []
+    self._queries = {}
+    inf = gzip.open(self.index_file)
+    z = 0
+    for line in inf:
+        f = line.rstrip("\n").split("\t")
+        name = f[0]
+        num = None
+        if name not in self._num_to_name:
+          self._num_to_name[z] = name
+          self._name_to_num[name] = z
+          num = z
+          z+=1
+        else:
+          num = self._name_to_num[name]
+        coord = [num,int(f[2]),int(f[3])]
+        rng = None
+        if f[1] != '':
+          rng = GenomicRange(range_string=f[1])
+          rng.set_payload(coord)
+          self._ranges.append(rng)
+        if num not in self._queries:
+          self._queries[num] = []
+        self._queries[num].append(coord+[rng])
+    inf.close()
+    return
+  def get_coords_by_name(self,name):
+    return [[x[1],x[2]] for x in self._queries[self._name_to_num[name]]]
 
 class BAMFile:
-  def __init__(self,filename,blockStart=None,innerStart=None,cnt=None):
+  def __init__(self,filename,blockStart=None,innerStart=None,cnt=None,skip_index=False,index_obj=None,index_file=None):
     self.path = filename
     self.fh = BGZF(filename)
     # start reading the bam file
@@ -109,20 +178,40 @@ class BAMFile:
     self._read_top_header()
     self.ref_names = []
     self.ref_lengths = {}
+    self.index = None
     self._read_reference_information()
+    #prepare index
+    if not skip_index:
+      if index_obj: self.index = index_obj
+      elif index_file: 
+        self.index = BAMIndex(index_file)
+      elif os.path.exists(self.path+'.jwx'):
+        self.index = BAMIndex(self.path+'.jwx')
+      else: # we make an index
+        b2 = BAMFile(self.path,skip_index=True)
+        of = None
+        try:
+          of = gzip.open(self.path+'.jwx','w')
+        except IOError:
+          sys.sterr.write("ERROR: could not find or create index\n")
+          sys.exit()
+        for e in b2:
+          rng = e.get_target_range()
+          if rng: of.write(e.value('qname')+"\t"+rng.get_range_string()+"\t"+str(e.get_block_start())+"\t"+str(e.get_inner_start())+"\n")
+          else: of.write(e.value('qname')+"\t"+''+"\t"+str(e.get_block_start())+"\t"+str(e.get_inner_start())+"\n")
+          self.index = BAMIndex(self.path+'.jwx')
+        of.close()
+    # prepare for specific work
     if self.path and blockStart and innerStart:
       self.fh.seek(blockStart,innerStart)
-    
 
   def __iter__(self):
     return self
-
   def next(self):
     e = self.read_entry()
     if not e:
       raise StopIteration
     else: return e
-
   def read_entry(self):
     bstart = self.fh.get_block_start()
     innerstart = self.fh.get_inner_start()
@@ -132,6 +221,12 @@ class BAMFile:
     #print 'block_size '+str(block_size)
     bam = BAM(self.fh.read(block_size),self.ref_names,fileName=self.path,blockStart=bstart,innerStart=innerstart)
     return bam
+
+  def get_query(self,name):
+    for coord in self.index.get_coords_by_name(name):
+      b2 = BAMFile(self.path,blockStart=coord[0],innerStart=coord[1],index_obj=self.index)
+      return b2.read_entry()
+    
   def _read_reference_information(self):
     for n in range(self.n_ref):
       l_name = struct.unpack('<i',self.fh.read(4))[0]
@@ -139,7 +234,6 @@ class BAMFile:
       l_ref = struct.unpack('<i',self.fh.read(4))[0]
       self.ref_lengths[name] = l_ref
       self.ref_names.append(name)
-
   def _read_top_header(self):
     magic = self.fh.read(4)
     l_text = struct.unpack('<i',self.fh.read(4))[0]
@@ -149,42 +243,44 @@ class BAMFile:
 def _parse_bam_data_block(bin_in,ref_names):
   v = {}
   data = StringIO(bin_in)
-  refID = struct.unpack('<i',data.read(4))[0]
-  v['rname'] = ref_names[refID]
-  pos = struct.unpack('<i',data.read(4))[0]
-  v['pos'] = pos+1
+  v['rname'] = ref_names[struct.unpack('<i',data.read(4))[0]] #refID to check in ref names
+  v['pos'] = struct.unpack('<i',data.read(4))[0] + 1 #POS
   bin_mq_nl = struct.unpack('<I',data.read(4))[0]
   bin =  bin_mq_nl >> 16 
-  mapq = (bin_mq_nl & 0xFF00) >> 8
-  v['mapq'] = mapq
-  l_read_name = bin_mq_nl & 0xFF
-  flag_nc = struct.unpack('<I',data.read(4))[0]
-  flag = flag_nc >> 16
-  v['flag'] = flag
-  n_cigar_op = flag_nc & 0xFF
+  v['mapq'] = (bin_mq_nl & 0xFF00) >> 8 #mapq
+  l_read_name = bin_mq_nl & 0xFF #length of qname
+  flag_nc = struct.unpack('<I',data.read(4))[0] #flag and n_cigar_op
+  v['flag'] = flag_nc >> 16
+  n_cigar_op = flag_nc & 0xFFFF
   l_seq = struct.unpack('<i',data.read(4))[0]
-  next_refID = struct.unpack('<i',data.read(4))[0]
-  v['rnext'] = ref_names[next_refID]
-  next_pos = struct.unpack('<i',data.read(4))[0]
-  v['pnext'] = next_pos+1
+  v['rnext'] = ref_names[struct.unpack('<i',data.read(4))[0]] #next_refID in ref_names
+  v['pnext'] = struct.unpack('<i',data.read(4))[0]+1 #pnext
   tlen = struct.unpack('<i',data.read(4))[0]
   v['tlen'] = tlen
-  read_name = data.read(l_read_name).rstrip('\0')
-  v['qname'] = read_name
-  cigar_bytes = data.read(n_cigar_op*4)
-  v['cigar_bytes'] = cigar_bytes
-  seq_bytes = data.read((l_seq+1)/2)
-  v['seq_bytes'] = seq_bytes
-  qual_bytes = data.read(l_seq)
-  v['qual_bytes'] = qual_bytes
-  extra_bytes = data.read()
-  v['extra_bytes'] = extra_bytes
+  v['qname'] = data.read(l_read_name).rstrip('\0') #read_name or qname
+  #print 'n_cigar_op '+str(n_cigar_op)
+  v['cigar_bytes'] = data.read(n_cigar_op*4)
+  #print 'cigar bytes '+str(len(v['cigar_bytes']))
+  v['seq_bytes'] = data.read((l_seq+1)/2)
+  v['qual_bytes'] = data.read(l_seq)
+  v['extra_bytes'] = data.read()
+  #last second tweak
+  if v['rnext'] == v['rname']: v['rnext'] = '='
   return v
 
 def _bin_to_qual(qual_bytes):
-  return ''.join([chr(struct.unpack('B',x)[0]+33) for x in qual_bytes])
+  #print 'qual note' +str(struct.unpack('<B',qual_bytes[1])[0])
+  if struct.unpack('<B',qual_bytes[1])[0] == 0xFF: return '*'
+  #print qual_bytes
+  #try:
+  qual = ''.join([chr(struct.unpack('<B',x)[0]+33) for x in qual_bytes])
+  #except:
+  #  return '*'
+  return qual
+
 def _bin_to_seq(seq_bytes):
   global _bam_char
+  #print len(seq_bytes)
   seq = ''.join([''.join([''.join([chr(z+97).translate(_bam_char) for z in  [y>>4,y&0xF]]) for y in struct.unpack('<B',x)]) for x in seq_bytes]).rstrip('=')
   return seq
 def _bin_to_cigar(cigar_bytes):
@@ -194,6 +290,7 @@ def _bin_to_cigar(cigar_bytes):
   cigar_array = [[c >> 4, str(c &0xF).translate(_bam_ops)] for c in cigar_packed]
   cigar_seq = ''.join([''.join([str(x[0]),x[1]]) for x in cigar_array])
   return [cigar_array,cigar_seq]
+
 #Pre all the reamining bytes of an entry
 #Post an array of 
 # 1. A dict keyed by Tag with {'type':,'value':} where value is a string unless type is i
@@ -318,10 +415,3 @@ class BGZF:
       sys.stderr.write("ERROR crc fail\n")
       sys.exit()
     return {'block_size':block_size, 'data':data}
-    #print crc
-    #print len(data)
-    #print expected_crc
-    #print expected_size
-    #print 'hello'
-
-#Some helper functions for reading binary
