43a815a774806e4c6e7cc6800bb155745a6eedb3
Jason L Weirather <jason.weirather@gmail.com>
2016-04-23 21:57:36 -0500

    first stage of multi processing

--- iron/utilities/fastq_bgzf_index.py
+++ iron/utilities/fastq_bgzf_index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys, argparse, StringIO, re, gzip
-from multiprocessing import Pool, cpu_count, Queue
+from multiprocessing import Pool, cpu_count, Queue, Lock
 from Bio.Format.BGZF import is_bgzf, reader as BGZF_reader, get_block_bounds
 from Bio.Format.Fastq import FastqEntry
 
@@ -14,7 +14,7 @@ from Bio.Format.Fastq import FastqEntry
 
 # global
 blocks = {}
-ncount = 1
+glock = Lock()
 of = None
 def main():
   global blocks
@@ -31,6 +31,16 @@ def main():
   sys.stderr.write("scanning for new lines\n")
   z = 0
   scan_for_newlines(bs,args)
+  # now need to clean out non start lines
+  ncount = 0
+  bnames = sorted(blocks.keys())
+  for bname in bnames:
+     v = []
+     #print blocks[bname]
+     for i in range(len(blocks[bname])):
+       if ncount%4 == 0: v.append(blocks[bname][i])
+       ncount += 1
+     blocks[bname] = v
   sys.stderr.write("Traverse blocks and writing index\n")
   z = 0
   traverse_and_write(blocks,args)
@@ -41,14 +51,14 @@ def traverse_and_write(blocks,args):
   z = 0
   of = gzip.open(args.input_file+'.bgi','w')
   bnames = sorted(blocks.keys())
-  chunk_size = 100
+  chunk_size = 1000
   for block_set in [bnames[x:x+chunk_size] for x in range(0,len(bnames),chunk_size)]:
-    ready_set = [[x,blocks[x][0][0],[y[1] for y in blocks[x]]] for x in block_set]
+    ready_set = [[x,blocks[x][0][0],[y[1] for y in blocks[x]]] for x in block_set if len(blocks[x]) > 0]
     #block set is a block, 
     sys.stderr.write(str(n)+'/'+str(len(bnames))+"\r")
     n += chunk_size
     z += 1
-    v = prepare_blocks(ready_set,args,1)
+    v = prepare_blocks(ready_set,args,z)
     write_results(v)
 
   sys.stderr.write("\n")
@@ -120,13 +130,22 @@ def scan_for_newlines(bs,args):
   z = 0
   # do them in chunks
   #for bounds in bs:
-  chunk_size = 100
+  chunk_size = 1000
+  if args.threads > 1:
+    p = Pool(processes=args.threads)
   for bounds_blocks in [bs[x:x+chunk_size] for x in range(0,len(bs),chunk_size)]:
+    if args.threads > 1:
+      p.apply_async(get_nls,args=(bounds_blocks,args.input_file,z),callback=do_nls_output)
+      #do_nls_output(v)
+    else:
       v = get_nls(bounds_blocks,args.input_file,z)
       do_nls_output(v)
-      sys.stderr.write(str(z)+'/'+str(len(bs))+"\r")
-      z += chunk_size
+    sys.stderr.write(str(z)+'/'+str(len(bs))+"\r")
+    z += chunk_size
   sys.stderr.write("\n")
+  if args.threads > 1:
+    p.close()
+    p.join()
 
 def get_nls(bounds_blocks,fname,i):
   breaks = []  
@@ -142,18 +161,22 @@ def get_nls(bounds_blocks,fname,i):
     nls = [m.start() for m in p.finditer(ubytes)]
     for j in range(len(nls)):
       breaks.append([bounds[0],bounds[1],nls[j],i])
+    s.close()
   return breaks
 
 def do_nls_output(results):
   global blocks
-  global ncount
+  #global ncount
+  global glock
+  glock.acquire()
   for e in results:
-      useval = False
-      if ncount%4 == 0: useval = True
-      ncount += 1
-      if not useval: continue
+      #useval = False
+      #if ncount%4 == 0: useval = True
+      #ncount += 1
+      #if not useval: continue
       if e[0] not in blocks: blocks[e[0]] = []
       blocks[e[0]].append([e[1],e[2]])
+  glock.release()
     
 def readfastq(reader):
   buffer = ''
