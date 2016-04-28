f51568278b89e41704e9bc58f0ae89be70d90bd9
Jason L Weirather <jason.weirather@gmail.com>
2016-04-28 13:24:58 -0500

    change fastq to tsv

--- iron/utilities/fastq_to_tsv.pl
+++ iron/utilities/fastq_to_tsv.pl
@@ -9,8 +9,7 @@ use strict;
 
 #if(scalar(@ARGV) == 0) { die "<input filename (optional)> <output filename (optional)>\n"; }
 # usage: <input filename (optional)> <output filename (optional)>
-if(scalar(@ARGV) == 0) { 
-  while(my $line1 = <STDIN>) {
+while(my $line1 = <STDIN>) {
     chomp($line1);
     my $name;
     if($line1=~/^@(.+)/) { $name = $1; } else { die "bad header1 format\n"; }
@@ -20,39 +19,4 @@ if(scalar(@ARGV) == 0) {
     if($line3=~/^\+/) { } else { die "bad header2 format\n"; }
     chomp(my $line4 = <STDIN>);
     print "$name\t$line2\t$line3\t$line4\n";
-  }
-} else {
-  my $infile = shift @ARGV;
-  if(scalar(@ARGV) == 0) {
-    open(INF,$infile) or die;
-    my $i = 0;
-    while(my $line1 = <INF>) {
-      chomp($line1);
-      my $name;
-      if($line1=~/^@(.+)/) { $name = $1; } else { die "bad header1 format\n"; }
-      chomp(my $line2 = <INF>);
-      chomp(my $line3 = <INF>);
-      if($line3=~/^\+/) { } else { die "bad header2 format\n"; }
-      chomp(my $line4 = <INF>);
-      print ">$name\n$line2\n";
-    }
-    close INF;
-  } else { 
-    my $outfile = shift @ARGV;
-    open(INF,$infile) or die;
-    open(OF,">$outfile") or die;
-    my $i = 0;
-    while(my $line1 = <INF>) {
-      chomp($line1);
-      my $name;
-      if($line1=~/^@(.+)/) { $name = $1; } else { die "bad header1 format\n"; }
-      chomp(my $line2 = <INF>);
-      chomp(my $line3 = <INF>);
-      if($line3=~/^\+/) { } else { die "bad header2 format\n"; }
-      chomp(my $line4 = <INF>);
-      print OF ">$name\n$line2\n";
-    }
-    close INF;
-    close OF;
-  }
 }
