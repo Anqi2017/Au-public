9aea813c55085086c64f08f6c17b10cf66e10b71
Jason L Weirather <jason.weirather@gmail.com>
2016-06-09 09:42:51 -0500

    make a fastq into an unmapped sam

---
+++ iron/utilities/fastq_to_unmapped_sam.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys, argparse
+from Bio.Format.Fasta import FastaData
+from Bio.Format.Fastq import FastqHandle
+
+def main():
+  parser = argparse.ArgumentParser(description="",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('input',help="Use - for STDIN")
+  parser.add_argument('-r','--reference',help="reference genome FASTA")
+  
+  args = parser.parse_args()
+  ref = {}
+  if args.reference:
+    ref = FastaData(open(args.reference,'rb').read())
+  if args.input == '-':
+    args.input = sys.stdin
+  else: args.input = open(args.input)
+  
+  
+  h1 = '@HD	VN:1.0	SO:unsorted'
+  h2 = '@PG	ID:FA2UN	PN:FA2UN	VN:2016-06-09	CL:'+' '.join(sys.argv)
+  print h1
+  print h2
+  if ref:
+    for chr in sorted(ref.keys()):
+      print "@SQ\tSN:"+chr+"\t"+'LN:'+str(len(ref[chr]))
+  inf = FastqHandle(args.input)
+  for e in inf:
+    o =  ''
+    o += e.name+"\t"
+    o += "4\t"
+    o += "*\t"
+    o += "0\t"
+    o += "0\t"
+    o += "*\t"
+    o += "*\t"
+    o += "0\t"
+    o += "0\t"
+    o += e.seq+"\t"
+    o += e.qual+"\t"
+    o += "XO:Z:NM"
+    print o
+
+if __name__=="__main__":
+  main()
