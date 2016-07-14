8336e18e8a954e691acb4fead463bdf9e5c432a1
Jason L Weirather <jason.weirather@gmail.com>
2016-07-14 11:34:20 -0500

    add gpd support to pacbio renaming

--- iron/utilities/rename_to_pacbio.py
+++ iron/utilities/rename_to_pacbio.py
@@ -8,6 +8,7 @@ def main():
   group = parser.add_mutually_exclusive_group(required=True)
   group.add_argument('--fasta',action='store_true')
   group.add_argument('--fastq',action='store_true')
+  group.add_argument('--gpd',action='store_true')
   parser.add_argument('--output_table',help='save coversion to file')
   parser.add_argument('-o','--output')
   args = parser.parse_args()
@@ -16,14 +17,29 @@ def main():
   else: args.input= open(args.input)
 
   if args.output: args.output = open(args.output,'w')
+  if args.output_table:  args.output_table= open(args.output_table,'w')
   else: args.output = sys.stdout
+  if args.gpd:
+    z = 0
+    for line in args.input:
+        f = line.rstrip().split("\t")
+        z+=1
+        name = 'm150101_010101_11111_c111111111111111111_s1_p0/'+str(z)+'/ccs'
+        if args.output_table: args.output_table.write(f[0]+"\t"+name+"\n")
+        f[0] = name
+        f[1] = name
+        args.output.write("\t".join(f)+"\n")
+    args.output.close()
+    if args.output_table:
+      args.output_table.close()
+    return
+
 
   if args.fasta:
     args.input = FastaHandleReader(args.input)
   elif args.fastq:
     args.input = FastqHandleReader(args.input)
   z = 0
-  if args.output_table:  args.output_table= open(args.output_table,'w')
   while True:
     e = args.input.read_entry()
     if not e: break
@@ -34,5 +50,7 @@ def main():
     elif args.fasta:
       args.output.write('>'+name+"\n"+e['seq']+"\n")
     if args.output_table: args.output_table.write(e['name']+"\t"+name+"\n")
+  args.output.close()
+  if args.output_table: args.output_table.close()
 if __name__=="__main__":
   main()
